acfd70ee47ef4aa22f47164b2a27a916e2931c13
BluSkreen <jasonduran303@gmail.com>
2023-02-03 10:25:30 -0700

    project cards

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,33 +1,60 @@
 import HeroText from "../components/HeroText";
 
+const callouts = [
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '#',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '#',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '#',
+  },
+]
+
 const Projects = () => {
     return (
         <section className="mt-44 w-full h-full flex justify-center">
-            <div className="w-full h-full max-w-[70rem] max-h-[20rem] flex flex-col">
-                <div className="w-full h-full">
-                    <HeroText size="small"/>
+    <div className="flex content-center">
+      <div className="mx-auto max-w-7xl">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-2xl font-bold text-gray-100">Collections</h2>
+
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+                  <img
+                    src={callout.imageSrc}
+                    alt={callout.imageAlt}
+                    className="h-full w-full object-cover object-center"
+                  />
                 </div>
-                <a
-                    href="#"
-                    className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-                >
-                    <img
-                        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="/docs/images/blog/image-4.jpg"
-                        alt=""
-                    />
-                    <div className="flex flex-col justify-between p-4 leading-normal">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Noteworthy technology acquisitions 2021
-                        </h5>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Here are the biggest enterprise technology
-                            acquisitions of 2021 so far, in reverse
-                            chronological order.
-                        </p>
-                    </div>
-                </a>
-            </div>
+                <h3 className="mt-6 text-sm text-gray-400">
+                  <a href={callout.href}>
+                    <span className="absolute inset-0" />
+                    {callout.name}
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-100">{callout.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
         </section>
     );
 };
